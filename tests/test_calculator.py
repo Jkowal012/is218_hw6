f8355dbb30730b20a713dc8e291abae4038fa071
Jkowal012 <jk262@njit.edu>
2024-10-01 19:52:32 -0400

    test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,19 @@
 #Test for calculator function
-from calculator import Calculator
+from calculator.calculation import Calculation
+#from calculator import Calculator
 
 def test_add():
     #Testing addition function
-    assert Calculator.add(4,4) == 8
+    assert Calculation.add(4,4) == 8
 
 def test_sub():
     #Testing subtraction function
-    assert Calculator.subtract(10,2) == 8
+    assert Calculation.subtract(10,2) == 8
 
 def test_mult():
     #Testing multiplication function
-    assert Calculator.multiply(10,10) == 100
+    assert Calculation.multiply(10,10) == 100
 
 def test_div():
     #Testing division function
-    assert Calculator.divide(5,5) == 1
+    assert Calculation.divide(5,5) == 1
