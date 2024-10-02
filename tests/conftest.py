861993a7137e556392fd6a9131b3a168802d717d
Jkowal012 <jk262@njit.edu>
2024-10-01 20:36:03 -0400

    fixing directory path

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(~/IS218/is218_hw3), '..')))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,19 @@
 #Test for calculator function
-from calculator.calculation import Calculation
+from calculator.operations import add, multiply, subtract, divide
 #from calculator import Calculator
 
 def test_add():
     #Testing addition function
-    assert Calculation.add(4,4) == 8
+    assert add(4,4) == 8
 
 def test_sub():
     #Testing subtraction function
-    assert Calculation.subtract(10,2) == 8
+    assert subtract(10,2) == 8
 
 def test_mult():
     #Testing multiplication function
-    assert Calculation.multiply(10,10) == 100
+    assert multiply(10,10) == 100
 
 def test_div():
     #Testing division function
-    assert Calculation.divide(5,5) == 1
+    assert divide(5,5) == 1
