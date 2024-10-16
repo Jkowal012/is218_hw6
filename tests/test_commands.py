f6c5a7a9f7c7e676f644a1910f1680de9ee2b770
Jkowal012 <jk262@njit.edu>
2024-10-16 18:52:12 -0400

    updated for logging

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,46 +1,48 @@
 # tests/test_commands.py
 
 import pytest
+import logging
 from app.plugins.add import AddCommand
 from app.plugins.subtract import SubtractCommand
 from app.plugins.multiply import MultiplyCommand
 from app.plugins.divide import DivideCommand
+from app.commands import CommandHandler
 
-def test_add_command(capsys):
+def test_add_command(caplog):
     command = AddCommand()
-    command.execute('2', '3')
-    captured = capsys.readouterr()
-    assert "Result: 5.0" in captured.out
+    with caplog.at_level(logging.INFO):
+        command.execute('2', '3')
+    assert "Result: 5.0" in caplog.text
 
-def test_add_command_random(capsys, faker):
+def test_add_command_random(caplog, faker):
     command = AddCommand()
     num1 = str(faker.random_number())
     num2 = str(faker.random_number())
-    command.execute(num1, num2)
-    captured = capsys.readouterr()
     expected_result = float(num1) + float(num2)
-    assert f"Result: {expected_result}" in captured.out
+    with caplog.at_level(logging.INFO):
+        command.execute(num1, num2)
+    assert f"Result: {expected_result}" in caplog.text
 
-def test_subtract_command(capsys):
+def test_subtract_command(caplog):
     command = SubtractCommand()
-    command.execute('5', '3')
-    captured = capsys.readouterr()
-    assert "Result: 2.0" in captured.out
+    with caplog.at_level(logging.INFO):
+        command.execute('5', '3')
+    assert "Result: 2.0" in caplog.text
 
-def test_multiply_command(capsys):
+def test_multiply_command(caplog):
     command = MultiplyCommand()
-    command.execute('2', '3')
-    captured = capsys.readouterr()
-    assert "Result: 6.0" in captured.out
+    with caplog.at_level(logging.INFO):
+        command.execute('2', '3')
+    assert "Result: 6.0" in caplog.text
 
-def test_divide_command(capsys):
+def test_divide_command(caplog):
     command = DivideCommand()
-    command.execute('6', '3')
-    captured = capsys.readouterr()
-    assert "Result: 2.0" in captured.out
+    with caplog.at_level(logging.INFO):
+        command.execute('6', '3')
+    assert "Result: 2.0" in caplog.text
 
-def test_divide_by_zero_command(capsys):
+def test_divide_by_zero_command(caplog):
     command = DivideCommand()
-    command.execute('6', '0')
-    captured = capsys.readouterr()
-    assert "Error: Cannot divide by zero" in captured.out
+    with caplog.at_level(logging.ERROR):
+        command.execute('6', '0')
+    assert "Error: Cannot divide by zero" in caplog.text
