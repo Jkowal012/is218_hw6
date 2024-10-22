9b4dca3d91b9a91855a9239e02260a8d8dd95529
Jkowal012 <jk262@njit.edu>
2024-10-22 14:47:38 -0400

    fixed logging into logging.txt issue

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,36 @@ import logging
 import pkgutil
 import importlib
 
+# Load environment variables from .env
 load_dotenv()
 
-# Configure logging at the module level
+# Remove all handlers associated with the root logger object
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+# Configure logging
 log_level = os.environ.get('LOG_LEVEL', 'WARNING').upper()
-logging.basicConfig(
-    level=getattr(logging, log_level, logging.WARNING),
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+numeric_level = getattr(logging, log_level, logging.WARNING)
+
+# Create a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Create handlers
+console_handler = logging.StreamHandler()
+console_handler.setLevel(numeric_level)
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logging.txt', mode='w')  # 'w' to overwrite the file each time
+file_handler.setLevel(numeric_level)
+file_handler.setFormatter(formatter)
+
+# Get the root logger
+logger = logging.getLogger()
+logger.setLevel(numeric_level)
+
+# Add handlers to the logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 from app.commands import CommandHandler
 from app.commands import Command
@@ -22,14 +44,10 @@ class App:
     def __init__(self):
         # Access settings from environment or use default values
         self.settings = os.environ.copy()
-        self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
+        self.settings.setdefault('ENVIRONMENT', 'DEVELOPMENT')
 
         # Configure other app components
         self.command_handler = CommandHandler()
-
-        # Configure logging within the class, if needed
-        log_level = self.settings.get('LOG_LEVEL', 'WARNING').upper()
-        logging.getLogger().setLevel(getattr(logging, log_level, logging.WARNING))
 
     def get_environment_variable(self, envvar: str = 'ENVIRONMENT'):
         return self.settings.get(envvar)
@@ -50,6 +68,7 @@ class App:
 
     def start(self):
         self.load_plugins()
-        print("Type 'exit' to exit.")
+        logging.info("Type 'exit' to exit.")
         while True:
-            self.command_handler.execute_command(input(">>> ").strip())
+            user_input = input(">>> ").strip()
+            self.command_handler.execute_command(user_input)
